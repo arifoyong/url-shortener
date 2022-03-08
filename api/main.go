8337048a629309880a3d9fa250ad208adaad6076
arifoyong <arifoyong@gmail.com>
2022-03-08 22:58:29 +0800

    Input url on frontend

--- api/main.go
+++ api/main.go
@@ -4,20 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/arifoyong/url-shortening/routes"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/test", func(c *fiber.Ctx) error {
-		fmt.Println("minute", 60/time.Minute)
-		fmt.Println("nanosecond", 60/time.Nanosecond)
-		fmt.Println("nanosecond", 60/time.Nanosecond/time.Minute)
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{"msg": "ok"})
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"msg": "ok from test"})
 	})
 	app.Get("/:url", routes.ResolveURL)
 	app.Post("/api/v1", routes.ShortenURL)
@@ -30,6 +28,10 @@ func main() {
 	}
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3030",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	setupRoutes(app)
 	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
